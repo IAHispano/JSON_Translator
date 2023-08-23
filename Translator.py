4040acd6fe39b2ff6deed7082af76231377e4a78
blaise <133521603+blaise-tk@users.noreply.github.com>
2023-08-23 15:10:30 +0200

    Updated to the new version

--- Translator.py
+++ Translator.py
@@ -1,29 +1,40 @@
 import json
 from googletrans import Translator
-from tqdm import tqdm  # Importa la librería tqdm para la barra de progreso
+from tqdm import tqdm
+import os
 
 def translate_values(json_data, input_language, target_language):
     translator = Translator()
     translated_data = {}
 
-    for key, value in tqdm(json_data.items(), desc="Traduciendo", ncols=100):
-        translation = translator.translate(value, src=input_language, dest=target_language)
-        translated_data[key] = translation.text
+    for key, value in tqdm(json_data.items(), desc=f"Translating to {target_language}", ncols=100):
+        try:
+            translation = translator.translate(value, src=input_language, dest=target_language)
+            translated_data[key] = translation.text
+        except Exception as e:
+            print(f"[ERROR] Translating '{value}' to {target_language}: {e}")
 
     return translated_data
 
-if __name__ == "__main__":  
-    input_file = "input.json"  # Nombre de tu archivo JSON de entrada
-    target_language = "ru"  # Código del idioma objetivo (ejemplo: "es" para español)
-    input_language = "zh-cn" # Código del idioma input (ejemplo: "zh-cn" para chino)
+if __name__ == "__main__":
+    input_file = "es_ES.json"  # Name of your input JSON file
+    input_language = "es"  # Input language code (e.g., "zh-cn" for Chinese)
+    target_languages = ["en", "zh-cn", "es", "hi", "ar", "bn", "pt", "ru", "ja", "pa", "de", "jv", "ms", "wu", "te",
+                   "vi", "ko", "fr", "mr", "ta", "ur", "tr", "it", "th", "gu", "fa", "pl", "uk", "ro", "nl", "hu"]
+
+    # Add more language codes as needed
 
     with open(input_file, "r", encoding="utf-8") as f:
         json_data = json.load(f)
 
-    translated_data = translate_values(json_data, input_language, target_language)
+    for target_language in target_languages:
+        translated_data = translate_values(json_data, input_language, target_language)
 
-    output_file = f"output_{target_language}.json"
-    with open(output_file, "w", encoding="utf-8") as f:
-        json.dump(translated_data, f, ensure_ascii=False, indent=2)
+        output_folder = "./output"
+        os.makedirs(output_folder, exist_ok=True)  # Create the output folder if it doesn't exist
 
-    print("Traducción completada y guardada en", output_file)
+        output_file = f"{output_folder}/{target_language}_{target_language.upper()}.json"
+        with open(output_file, "w", encoding="utf-8") as f:
+            json.dump(translated_data, f, ensure_ascii=False, indent=2)
+
+        print(f"Translation to {target_language} completed and saved to {output_file}")
