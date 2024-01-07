51a1f314d188b77195dfd760959da6268cf3fa5f
Blaise <133521603+blaise-tk@users.noreply.github.com>
2024-01-08 00:07:25 +0100

    new translator

--- README.md
+++ README.md
@@ -1,42 +1,12 @@
-# JSON Translator Script
+# Translator Script
 
-This script is designed to translate values from a JSON file to multiple target languages using the `translate` library for translation and provides an easy way to translate content across different languages.
-
-## Features
-
-- Translates values from a JSON file to multiple target languages.
-- Handles errors and displays them for easy debugging.
-- Creates a separate JSON file for each target language with translated content.
-- Automatically creates the output folder if it doesn't exist.
+This script translates a given JSON file from one language to multiple target languages using the `translators` library.
 
 ## Prerequisites
 
-Before using the script, make sure you have the following installed:
+- Python 3.x installed
+- Install the required dependencies using the following command:
 
-- Python (version 3.6 or later)
-- Required Python packages (install using `pip`):
-  - `translate`
-  - `tqdm`
-
-## Usage
-
-1. Place your input JSON file (e.g., `es_ES.json`) in the same directory as the script.
-2. Configure the script by modifying the following variables in the script:
-   - `input_file`: Name of your input JSON file.
-   - `input_language`: Language code of the input content (e.g., "es" for Spanish).
-   - `target_languages`: List of target language codes you want to translate to.
-3. Open a terminal/command prompt and navigate to the script directory.
-4. Run the script using the command:
+```bash
+pip install translators tqdm
 ```
-python Translator.py
-```
-6. The translated JSON files will be saved in the `output` folder.
-
-
-## Acknowledgments
-
-- This script was inspired by the need to translate content efficiently across languages.
-- Thanks to the developers of `translate` and `tqdm` for their useful libraries.
-
-Feel free to contribute, report issues, or suggest improvements!
-
--- Translator.py
+++ Translator.py
@@ -1,26 +1,63 @@
 import json
-from translate import Translator
+import translators as ts
 from tqdm import tqdm
 import os
 
+
 def translate_values(json_data, input_language, target_language):
-    translator = Translator(to_lang=target_language, from_lang=input_language)
     translated_data = {}
 
-    for key, value in tqdm(json_data.items(), desc=f"Translating to {target_language}", ncols=100):
+    for key, value in tqdm(
+        json_data.items(), desc=f"Translating to {target_language}", ncols=100
+    ):
         try:
-            translation = translator.translate(value)
+            translation = ts.translate_text(
+                value, from_language=input_language, to_language=target_language
+            )
             translated_data[key] = translation
         except Exception as e:
             print(f"[ERROR] Translating '{value}' to {target_language}: {e}")
 
     return translated_data
 
+
 if __name__ == "__main__":
+    # Config
     input_file = "en_US.json"
     input_language = "en"
-    target_languages = ["es", "zh-cn", "hi", "ar", "bn", "pt", "ru", "ja", "pa", "de", "jv", "ms", "wu", "te",
-                   "vi", "ko", "fr", "mr", "ta", "ur", "tr", "it", "th", "gu", "fa", "pl", "uk", "ro", "nl", "hu"]
+    output_folder = "./i18n/"
+    target_languages = [
+        "es",
+        "zh-cn",
+        "hi",
+        "ar",
+        "bn",
+        "pt",
+        "ru",
+        "ja",
+        "pa",
+        "de",
+        "jv",
+        "ms",
+        "wu",
+        "te",
+        "vi",
+        "ko",
+        "fr",
+        "mr",
+        "ta",
+        "ur",
+        "tr",
+        "it",
+        "th",
+        "gu",
+        "fa",
+        "pl",
+        "uk",
+        "ro",
+        "nl",
+        "hu",
+    ]
 
     with open(input_file, "r", encoding="utf-8") as f:
         json_data = json.load(f)
@@ -28,10 +65,11 @@ if __name__ == "__main__":
     for target_language in target_languages:
         translated_data = translate_values(json_data, input_language, target_language)
 
-        output_folder = "./output"
         os.makedirs(output_folder, exist_ok=True)
 
-        output_file = f"{output_folder}/{target_language}_{target_language.upper()}.json"
+        output_file = (
+            f"{output_folder}/{target_language}_{target_language.upper()}.json"
+        )
         with open(output_file, "w", encoding="utf-8") as f:
             json.dump(translated_data, f, ensure_ascii=False, indent=2)
 
